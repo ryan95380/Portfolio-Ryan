c96733c77fdb0edb38b9bb1c6562c22e9ca9b484
ryan95380 <ryanmambou270@gmail.com>
2025-09-25 10:27:11 +0200

    initialisation

---
+++ script.js
@@ -0,0 +1,21 @@
+// MARQUEE
+function Marquee(selector, speed) {
+  const Surface = document.querySelector(selector);
+  const nextRoll = Surface.innerHTML;
+  const firstElement = Surface.children[0];
+  let i = 0;
+  console.log(Surface);
+  console.log(firstElement);
+  Surface.insertAdjacentHTML('beforeend', nextRoll);
+  Surface.insertAdjacentHTML('beforeend', nextRoll);
+
+  setInterval(() => {
+    firstElement.style.marginLeft = `-${i}px`;
+    if (i > firstElement.clientWidth) {
+      i = 0 ;
+    }
+    i = i + speed;
+  });
+}
+ 
+window.addEventListener('load', Marquee('#marquee', 0.25))
